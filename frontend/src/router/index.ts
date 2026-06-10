4368223c70ddace3ba23e2bc41e6c26279044b21
Tilde Andreasson <tilde.andreasson@live.se>
2026-06-11 05:35:20 +0800

    Update index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,51 +1,25 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import SearchResults from '../views/SearchResults.vue'
+import ProductDetail from '../views/ProductDetail.vue'
+import Cart from '../views/Cart.vue'
+import Profile from '../views/Profile.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-  },
-  {
-    path: '/products',
-    name: 'Products',
-    component: () => import('../views/Products.vue'),
-  },
-  {
-    path: '/products/:id',
-    name: 'ProductDetail',
-    component: () => import('../views/ProductDetail.vue'),
-  },
-  {
-    path: '/cart',
-    name: 'Cart',
-    component: () => import('../views/Cart.vue'),
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/Register.vue'),
-  },
-  {
-    path: '/orders',
-    name: 'Orders',
-    component: () => import('../views/Orders.vue'),
-  },
-  {
-    path: '/orders/:id',
-    name: 'OrderDetail',
-    component: () => import('../views/OrderDetail.vue'),
-  },
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/search', name: 'Search', component: SearchResults },
+  { path: '/product/:id', name: 'ProductDetail', component: ProductDetail },
+  { path: '/cart', name: 'Cart', component: Cart },
+  { path: '/profile', name: 'Profile', component: Profile },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/register', name: 'Register', component: Register }
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  history: createWebHistory(),
+  routes
 })
 
 export default router
